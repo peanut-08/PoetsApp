95a1a8c53e14e34ff4c04c2fe53cc0e8f6522d2b
ladybacon <ladybaconshopping@gmail.com>
2022-05-31 15:02:21 +0200

    installeble

--- sw.js
+++ sw.js
@@ -7,3 +7,9 @@ self.addEventListener('install', evt => {
 self.addEventListener('activate', evt => {
     console.log('Service worker has been activated');
 });
+
+// fetch event
+self.addEventListener('fetch', evt => {
+    console.log('Fetch event', evt);
+});
+
